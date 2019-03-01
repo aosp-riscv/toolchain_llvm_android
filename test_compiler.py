28fd8cc731909b54d7cc6cd03f3909df828113cb
Yi Kong <yikong@google.com>
2019-03-01 15:09:25 -0800

    Add -flto-split-unit flag
    The new LLVM update requires '-flto-split-unit' to build ThinLTO. Add the flag unconditionally in test_compiler as a temporary workaround.
    
    To be reverted after the cflag is properly set in soong.
    
    Test: TreeHugger
    Change-Id: I5b4dd4a7486c8219473d24a4b92447fb8994db75

--- test_compiler.py
+++ test_compiler.py
@@ -35,6 +35,7 @@ DEFAULT_TIDY_CHECKS = ('*', '-readability-*', '-google-readability-*',
 # before we fix those warnings.
 DISABLED_WARNINGS = [
     '-Wno-error=defaulted-function-deleted',
+    '-flto-split-unit',
 ]
 
 
