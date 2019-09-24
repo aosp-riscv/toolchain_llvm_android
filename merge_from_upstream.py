dc9303e6e13532b9a8338819fb5c6bf40752a381
Nick Desaulniers <ndesaulniers@google.com>
2019-09-24 13:50:25 -0700

    merge_from_upstream.py: update to reflect monorepo
    Change-Id: I6529f80b9fd48744ef2a195bce1c971cec4ac4f6

--- merge_from_upstream.py
+++ merge_from_upstream.py
@@ -23,16 +23,6 @@ import sys
 
 from utils import *
 
-PROJECT_PATH = (
-    ('llvm', llvm_path()),
-    ('cfe', llvm_path('tools/clang')),
-    ('clang-tools-extra', llvm_path('tools/clang/tools/extra')),
-    ('compiler-rt', llvm_path('projects/compiler-rt')),
-    ('libcxx', llvm_path('projects/libcxx')),
-    ('libcxxabi', llvm_path('projects/libcxxabi')),
-    ('lld', llvm_path('tools/lld')),
-    ('openmp', llvm_path('projects/openmp')),)
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -57,124 +47,109 @@ def sync_upstream_branch(path):
 
 
 def merge_projects(revision, create_new_branch, dry_run):
-    project_sha_dict = {}
-    for (project, path) in PROJECT_PATH:
-        if not dry_run:
-            sync_upstream_branch(path)
-        sha = get_commit_hash(revision, path)
-        if sha is None:
-            return
-        project_sha_dict[project] = sha
-        print('Project %s git hash: %s' % (project, sha))
+    sha = ''
+    path = llvm_path()
+    if not dry_run:
+        sync_upstream_branch(path)
+    sha = get_commit_hash(revision, path)
+    if sha is None:
+        return
+    print('Project llvm-project git hash: %s' % sha)
 
-    for (project, path) in PROJECT_PATH:
-        sha = project_sha_dict[project]
-        if create_new_branch:
-            branch_name = 'merge-upstream-r%s' % revision
-            check_call_d(['repo', 'start', branch_name, '.'], cwd=path, dry_run=dry_run)
+    if create_new_branch:
+        branch_name = 'merge-upstream-r%s' % revision
+        check_call_d(['repo', 'start', branch_name, '.'],
+                     cwd=path,
+                     dry_run=dry_run)
 
-        # Get the info since the last tag, the format is
-        #   llvm-svn.[svn]-[number of changes since tag]-[sha of the current commit]
-        desc = subprocess.check_output(
-            ['git', 'describe', '--tags', '--long', '--match', 'llvm-svn.[0-9]*'],
-            cwd=path, universal_newlines=True).strip()
-        _, svnNum, numChanges, _ = desc.split('-')
+    # Get the info since the last tag, the format is
+    #   llvm-svn.[svn]-[number of changes since tag]-[sha of the current commit]
+    desc = subprocess.check_output(
+        ['git', 'describe', '--tags', '--long', '--match', 'llvm-svn.[0-9]*'],
+        cwd=path,
+        universal_newlines=True).strip()
+    _, svnNum, numChanges, _ = desc.split('-')
 
-        # Check changes since the previous merge point
-        reapplyList = []
-        for i in range(int(numChanges) - 1, -1, -1):
-            changeLog = subprocess.check_output(
-                ['git', 'show', 'HEAD~' + str(i), '--quiet', '--format=%h%x1f%B%x1e'],
-                cwd=path, universal_newlines=True
-            )
-            changeLog = changeLog.strip('\n\x1e')
-            patchSha, patchRev = parse_log(changeLog)
-            if patchRev is None or patchRev > revision:
-                reapplyList.append(patchSha)
+    # Check changes since the previous merge point
+    reapplyList = []
+    for i in range(int(numChanges) - 1, -1, -1):
+        changeLog = subprocess.check_output([
+            'git', 'show', 'HEAD~' + str(i), '--quiet', '--format=%h%x1f%B%x1e'
+        ],
+                                            cwd=path,
+                                            universal_newlines=True)
+        changeLog = changeLog.strip('\n\x1e')
+        patchSha, patchRev = parse_log(changeLog)
+        if patchRev is None or patchRev > revision:
+            reapplyList.append(patchSha)
 
-        # Reset to previous branch point, if necessary
-        if int(numChanges) > 0:
-            check_output_d(
-                [
-                    'git', 'revert', '--no-commit', '--no-merges',
-                    'llvm-' + svnNum + '...HEAD'
-                ],
-                cwd=path,
-                dry_run=dry_run
-            )
-            check_output_d(
-                ['git', 'commit', '-m revert to previous base llvm-' + svnNum],
-                cwd=path,
-                dry_run=dry_run
-            )
-
-        # Merge upstream revision
-        check_call_d(
-            [
-                'git', 'merge', '--quiet', sha, '-m',
-                'Merge %s for LLVM update to %d' % (sha, revision)
-            ],
+    # Reset to previous branch point, if necessary
+    if int(numChanges) > 0:
+        check_output_d([
+            'git', 'revert', '--no-commit', '--no-merges',
+            'llvm-' + svnNum + '...HEAD'
+        ],
+                       cwd=path,
+                       dry_run=dry_run)
+        check_output_d(
+            ['git', 'commit', '-m revert to previous base llvm-' + svnNum],
             cwd=path,
-            dry_run=dry_run
-        )
+            dry_run=dry_run)
 
-        # Tag the merge point
-        check_call_d(
-            ['git', 'tag', '-f', 'llvm-svn.' + str(revision)],
+    # Merge upstream revision
+    check_call_d([
+        'git', 'merge', '--quiet', sha, '-m',
+        'Merge %s for LLVM update to %d' % (sha, revision)
+    ],
+                 cwd=path,
+                 dry_run=dry_run)
+
+    # Tag the merge point
+    check_call_d(['git', 'tag', '-f', 'llvm-svn.' + str(revision)],
+                 cwd=path,
+                 dry_run=dry_run)
+
+    # Reapply
+    FNULL = open(os.devnull, 'w')
+    for sha in reapplyList:
+        subprocess.check_call(['git', '--no-pager', 'show', sha, '--quiet'],
+                              cwd=path)
+
+        # Check whether applying this change will cause conflict
+        ret_code = subprocess.call(
+            ['git', 'cherry-pick', '--no-commit', '--no-ff', sha],
             cwd=path,
-            dry_run=dry_run
-        )
+            stdout=FNULL,
+            stderr=FNULL)
+        subprocess.check_call(['git', 'reset', '--hard'],
+                              cwd=path,
+                              stdout=FNULL,
+                              stderr=FNULL)
 
-        # Reapply
-        FNULL = open(os.devnull, 'w')
-        for sha in reapplyList:
-            subprocess.check_call(
-                ['git', '--no-pager', 'show', sha, '--quiet'],
-                cwd=path
-            )
-
-            # Check whether applying this change will cause conflict
-            ret_code = subprocess.call(
-                ['git', 'cherry-pick', '--no-commit', '--no-ff', sha],
-                cwd=path,
-                stdout=FNULL,
-                stderr=FNULL
-            )
-            subprocess.check_call(
-                ['git', 'reset', '--hard'],
-                cwd=path,
-                stdout=FNULL,
-                stderr=FNULL
-            )
-
-            if ret_code != 0:
-                print 'Change cannot merge cleanly, please manual merge if needed'
-                print
-                keep_going = yes_or_no("Continue?", default=False)
-                if not keep_going:
-                    sys.exit(1)
-                continue
-
-            # Change can apply cleanly...
-            reapply = yes_or_no("Reapply change?", default=True)
-            if reapply:
-                check_call_d(
-                    ['git', 'cherry-pick', sha],
-                    cwd=path,
-                    stdout=FNULL,
-                    stderr=FNULL,
-                    dry_run=dry_run
-                )
-                # Now change the commit Change-Id.
-                check_call_d(
-                    ['git', 'commit', '--amend'],
-                    cwd=path,
-                    dry_run=dry_run
-                )
-            else:
-                print 'Skipping ' + sha
-
+        if ret_code != 0:
+            print 'Change cannot merge cleanly, please manual merge if needed'
             print
+            keep_going = yes_or_no('Continue?', default=False)
+            if not keep_going:
+                sys.exit(1)
+            continue
+
+        # Change can apply cleanly...
+        reapply = yes_or_no('Reapply change?', default=True)
+        if reapply:
+            check_call_d(['git', 'cherry-pick', sha],
+                         cwd=path,
+                         stdout=FNULL,
+                         stderr=FNULL,
+                         dry_run=dry_run)
+            # Now change the commit Change-Id.
+            check_call_d(['git', 'commit', '--amend'],
+                         cwd=path,
+                         dry_run=dry_run)
+        else:
+            print 'Skipping ' + sha
+
+        print
 
         print
 
