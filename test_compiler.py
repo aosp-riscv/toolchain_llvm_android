96365fd804523021c25110e01ca00e5641fd9703
Pirama Arumuga Nainar <pirama@google.com>
2020-02-07 15:55:17 -0800

    In a debug build, use stage1 compiler to build runtimes
    Bug: http://b/149056576
    
    In go/ab/aosp-llvm-toolchain, the linx_debug builds are taking 4 hours.
    This is because runtimes are built with the debug compiler.  This is
    slow on developer workstations as well.
    
    Instead, stage1 to build the runtimes in this scenario.  To do this, we
    should build the ARM and AArch64 backends during stage1.
    
    With this change, I can get a debug build on my workstation in 1h5m.
    
    Test: build.py --debug, presubmit
    Change-Id: If42becf3f4af6d8e009989bece2957099e545f5b

--- test_compiler.py
+++ test_compiler.py
@@ -309,7 +309,7 @@ def build_clang(instrumented=False, pgo=True):
 
     # LLVM tool llvm-profdata from stage1 is needed to merge the collected
     # profiles.  Build all LLVM tools if building instrumented stage2
-    build.build_stage1(stage1_install, build_name='dev',
+    build.build_stage1(stage1_install, build_name='dev', build.STAGE1_TARGETS,
                        build_llvm_tools=instrumented)
 
     profdata = None
