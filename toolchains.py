aefa18342dc7b74f00042e94758df4e6587b3e9c
Haibo Huang <hhb@google.com>
2020-03-13 21:23:19 -0700

    Refactor build script
    Introduce class hierarchies for Toolchain and Builders. Combine
    redundant code.
    
    Test: Compare out/stage1/CMakeCache.txt before and after change.
    Change-Id: I1d08e107695445bc1eca36bd0a86e8e0abafa68d

---
+++ toolchains.py
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""APIs for accessing toolchains."""
+
+from pathlib import Path
+
+import constants
+import hosts
+import paths
+
+class Toolchain:
+    """Base toolchain."""
+
+    cc: Path  # pylint: disable=invalid-name
+    cxx: Path
+    lib_path: Path
+
+
+class _HostToolchain(Toolchain):
+    """Base toolchain that compiles host binary."""
+
+    def __init__(self, path: Path) -> None:
+        self.path = path
+
+    @property
+    def cc(self) -> Path:
+        return self.path / 'bin' / 'clang'
+
+    @property
+    def cxx(self) -> Path:
+        return self.path / 'bin' / 'clang++'
+
+    @property
+    def lib_path(self) -> Path:
+        return self.path / 'lib64'
+
+
+class PrebuiltToolchain(_HostToolchain):
+    """A prebuilt toolchain used to build stage1."""
+
+    def __init__(self) -> None:
+        host = hosts.build_host()
+        clang_path: Path = self.clang_prebuilt_path(host)
+        super().__init__(clang_path)
+
+    @staticmethod
+    def clang_prebuilt_path(host: hosts.Host) -> Path:
+        """Returns the path to prebuilt clang toolchain."""
+        return (paths.ANDROID_DIR / 'prebuilts' / 'clang' / 'host' /
+                host.os_tag / constants.CLANG_PREBUILT_VERSION)
