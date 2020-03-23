b560c1b9a208b575bc0036e568dc1329e80fdc0e
Haibo Huang <hhb@google.com>
2020-03-23 12:17:49 -0700

    Fix do_test_compiler.py
    Change-Id: I16240827cd94334c0f69c8d5c30a966f9c9eaedc

--- do_test_compiler.py
+++ do_test_compiler.py
@@ -302,7 +302,6 @@ def test_device(android_base, clang_version, device, max_jobs, clean_output,
 
 
 def build_clang(instrumented=False, pgo=True):
-    stage1_install = utils.out_path('stage1-install')
     stage2_install = utils.out_path('stage2-install')
 
     # Clone sources to build the current version, with patches.
@@ -312,22 +311,28 @@ def build_clang(instrumented=False, pgo=True):
 
     # LLVM tool llvm-profdata from stage1 is needed to merge the collected
     # profiles.  Build all LLVM tools if building instrumented stage2
-    build.build_stage1(stage1_install, build_name='dev',
-                       stage1_targets=build.BASE_TARGETS,
-                       build_llvm_tools=instrumented)
+    stage1 = build.Stage1Builder()
+    stage1.build_name = 'dev'
+    stage1.svn_revision = 'dev'
+    stage1.build_llvm_tools = instrumented
+    stage1.debug_stage2 = False
+    stage1.build()
+    stage1_install = str(stage1.install_dir)
 
     profdata = None
     if pgo:
         long_version = build.extract_clang_long_version(stage1_install)
         profdata = build.pgo_profdata_file(long_version)
 
-    build.build_stage2(
-        stage1_install,
-        stage2_install,
-        build.ANDROID_TARGETS,
-        build_name='dev',
-        build_instrumented=instrumented,
-        profdata_file=profdata)
+    stage2 = build.Stage2Builder()
+    stage2.build_name = 'dev'
+    stage2.svn_revision = 'dev'
+    stage2.build_lldb = False
+    stage2.build_instrumented = instrumented
+    stage2.profdata_file = Path(profdata) if profdata else None
+    stage2.build()
+    stage2_install = str(stage2.install_dir)
+
     build.build_runtimes(stage2_install)
 
     build.package_toolchain(
