b7a0701438c66c7acf2384b2e927243fffc5a00d
Haibo Huang <hhb@google.com>
2020-07-22 10:55:13 -0700

    Adds an option to build windows toolchain with win_sdk
    Adds a new '--windows-sdk' option. If set to the path to windows SDKs, the sdk will be used to build windows llvm.
    
    This change didn't build and use libcxx (yet).
    
    Test: ./build.py --no-lto --no-build=linux --windows-sdk=/src/visualcpptools/win_sdk
    Bug: 162243795
    Change-Id: I4318e9d535ac9f0e311de5649837e443ba8c22d4

--- toolchains.py
+++ toolchains.py
@@ -34,6 +34,7 @@ class Toolchain:
 
     @property
     def cc(self) -> Path:  # pylint: disable=invalid-name
+        """Returns the path to c compiler."""
         return self.path / 'bin' / 'clang'
 
     @property
@@ -42,9 +43,18 @@ class Toolchain:
         return self.path / 'bin' / 'clang++'
 
     @property
+    def cl(self) -> Path:
+        """Returns the path to c++ compiler."""
+        return self.path / 'bin' / 'clang-cl'
+
+    @property
     def strip(self) -> Path:
         """Returns the path to llvm-strip."""
         return self.path / 'bin' / 'llvm-strip'
+
+    @property
+    def mt(self) -> Path:
+        return self.path / 'bin' / 'llvm-mt'
 
     @property
     def lib_dir(self) -> Path:
