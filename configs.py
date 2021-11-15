d3f1dacb82b3455425ac8731229b70cf5cee9361
luxufan <932494295@qq.com>
2021-11-15 18:37:34 +0800

    add -mno-relax option to riscv build

--- configs.py
+++ configs.py
@@ -328,6 +328,8 @@ class AndroidConfig(_BaseConfig):
         cflags.append(f'-B{toolchain_bin}')
         cflags.append('-ffunction-sections')
         cflags.append('-fdata-sections')
+        if self.ndk_arch == 'riscv64':
+            cflags.append('mno-relax')
         return cflags
 
     @property
@@ -348,6 +350,8 @@ class AndroidConfig(_BaseConfig):
     @property
     def cxxflags(self) -> List[str]:
         cxxflags = super().cxxflags
+        if self.ndk_arch == 'riscv64':
+            cxxflags.append('-mno-relax')
         if self.platform:
             # For the NDK, the sysroot has the C++ headers, but for the
             # platform, we need to add the headers manually.
